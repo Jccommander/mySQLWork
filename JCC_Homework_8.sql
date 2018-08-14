be691f606f161346a3318255a943ddf4f3c68314
JCCom <Jccommander@Johns-MacBook-Pro-2.local>
2018-08-14 12:59:46 -0400

    Continued work on SQL file, finished 6e, still need to do 7 and 8

--- JCC_Homework_8.sql
+++ JCC_Homework_8.sql
@@ -42,4 +42,31 @@ WHERE first_name = "GROUCHO" AND last_name = "WILLIAMS";
 UPDATE actor SET first_name = "GROUCHO" WHERE first_name = "HARPO";
 SET SQL_SAFE_UPDATES = 1;
 
+-- 5.a
+SHOW CREATE TABLE address;
 
+-- 6.a
+SELECT s.first_name, s.last_name, a.address FROM staff s
+JOIN address a ON
+s.address_id = a.address_id;
+
+-- 6.b
+SELECT  CONCAT(s.first_name, ' ', s.last_name) AS "Employee", sum(p.amount) AS "Total for August 2005" FROM staff s
+JOIN payment p ON
+s.staff_id = p.staff_id WHERE p.payment_date LIKE "2005-08%"
+GROUP BY CONCAT(s.first_name, ' ', s.last_name);
+
+-- 6.c
+SELECT f.title AS "Film Title", count(fa.actor_id) AS "Number of Actors" FROM film f
+JOIN film_actor fa ON
+f.film_id = fa.film_id GROUP BY f.title;
+
+-- 6.d
+SELECT f.title AS "Film Title", count(i.inventory_id) AS "Copies in Inventory" FROM film f
+JOIN inventory i ON
+f.film_id = i.film_id WHERE f.title = "HUNCHBACK IMPOSSIBLE";
+
+-- 6.e
+SELECT c.first_name, c.last_name, sum(p.amount) AS "Total Paid" FROM customer c
+JOIN payment p ON
+c.customer_id = p.customer_id GROUP BY c.first_name, c.last_name ORDER BY c.last_name;
